0227f534620d64593382273046e256f8558d0eb2
Andre H II <35673515+Nullified002@users.noreply.github.com>
2022-01-09 12:36:18 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,53 @@
+// All valid credit card numbers
+const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8];
+const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9];
+const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6];
+const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5];
+const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6];
+
+// All invalid credit card numbers
+const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5];
+const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3];
+const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4];
+const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5];
+const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4];
+
+// Can be either valid or invalid
+const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4];
+const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9];
+const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3];
+const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3];
+const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
+
+// An array of all the arrays above
+const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5];
+
+let arrAdd = 0;
+let invalids = []
+// Add your functions below:
+const validateCred = arr => {
+  //let luhn = arr.reverse(); //makes it so you can "iterate from right to left"
+  let checked = arr; // creates new  array so the original isn't modified.
+  console.log(checked);
+  let current = checked[i];
+for (i = checked.length - 1; i > 0; i-2) {// actually iterates backwards without reversing the array
+  
+  current * 2; // i-2 so it skips a digit after each iteration
+  if (current > 9) {current - 9};
+  arrAdd += current;
+  console.log("ArrAdd = " + arrAdd);
+}
+ return (arrAdd % 10 == 0);// you don't have to tell programs to return true. If your statement is true, it will return true. 
+};
+
+const findInvalidCards = batch => {
+  for (const arr of batch) {
+   if (validateCred(arr) != true) {
+     invalids.push(arr)
+   }}
+}
+
+validateCred(valid5);
+
+
+
